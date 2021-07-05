347f2d8c687783f36743f67fc9c41a7c8dfd6d7d
kinshuk-k <86929123+kinshuk-k@users.noreply.github.com>
2021-07-05 22:02:13 +0530

    Add files via upload

---
+++ Downloader/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDownloaded = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // downButton
+            // 
+            this.downButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.downButton.Location = new System.Drawing.Point(371, 82);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(106, 33);
+            this.downButton.TabIndex = 0;
+            this.downButton.Text = "&Download";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Downloader Application";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(26, 365);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(397, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // textUrl
+            // 
+            this.textUrl.Location = new System.Drawing.Point(22, 91);
+            this.textUrl.Name = "textUrl";
+            this.textUrl.Size = new System.Drawing.Size(343, 20);
+            this.textUrl.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Download url below:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(82, 129);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(32, 18);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "???";
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercent.Location = new System.Drawing.Point(390, 333);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(33, 18);
+            this.lblPercent.TabIndex = 7;
+            this.lblPercent.Text = "0 %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Downloaded:";
+            // 
+            // lblDownloaded
+            // 
+            this.lblDownloaded.AutoSize = true;
+            this.lblDownloaded.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownloaded.Location = new System.Drawing.Point(123, 206);
+            this.lblDownloaded.Name = "lblDownloaded";
+            this.lblDownloaded.Size = new System.Drawing.Size(41, 18);
+            this.lblDownloaded.TabIndex = 9;
+            this.lblDownloaded.Text = "0 Mb";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Speed";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpeed.Location = new System.Drawing.Point(123, 248);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(53, 18);
+            this.lblSpeed.TabIndex = 11;
+            this.lblSpeed.Text = "0 Mb/s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 423);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblDownloaded);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textUrl);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.downButton);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "File Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDownloaded;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSpeed;
+    }
+}
+
---
+++ Downloader/Form1.cs
@@ -0,0 +1,60 @@
+﻿using AltoHttp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        HttpDownloader httpDownloader;
+
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            string dir = @"D:\Test";
+            if(!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            httpDownloader = new HttpDownloader(textUrl.Text, dir);
+            httpDownloader.DownloadCompleted += HttpDownloader_DownloadCompleted;
+            httpDownloader.ProgressChanged += HttpDownloader_ProgressChanged;
+            httpDownloader.Start();
+        }
+
+        private void HttpDownloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = (int)e.Progress;
+            lblPercent.Text = $"{e.Progress.ToString("0.00")} %";
+            lblSpeed.Text = string.Format("{0} Mb/s", (e.SpeedInBytes / 1024d / 1024d).ToString("0.00"));
+            lblDownloaded.Text = string.Format("{0} Mb/s", (httpDownloader.TotalBytesReceived / 1024d / 1024d).ToString("0.00"));
+            lblStatus.Text = "Downloading..";
+        }
+
+        private void HttpDownloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                lblStatus.Text = "Finished";
+                lblPercent.Text = "100 %";
+            });
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
